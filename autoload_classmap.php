84ecbcfc8f0afd1ea60824cc51cf11f4a4dc67a9
Starx <mail@mrnepal.com>
2014-06-02 16:34:59 +0545

    Correct the Autoloader

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -1,5 +1,5 @@
 <?php
 return array(
     'nGen\Zfc\Module'                                    => __DIR__ . '/Module.php',
-    'nGen\Zfc\Mapper\ExtendedAbstractDbMapper'           => __DIR__ . '/src/nGenZfc/Mapper/ExtendedAbstractDbMapper.php',
+    'nGen\Zfc\Mapper\ExtendedAbstractDbMapper'           => __DIR__ . '/src/Zfc/Mapper/ExtendedAbstractDbMapper.php',
 );
